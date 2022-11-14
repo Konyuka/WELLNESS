1a209eaf49a982d6db08099c98e6936f5f5e221b
Michael Saiba <michaelsaiba84@gmail.com>
2022-11-14 17:39:59 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <div class="">
+        <NavBar />
+      </div>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="opacity-50 bg-hero-pattern h-screen bg-cover bg-center object-cover">
+  <!-- <div class=" bg-gradient-to-r from-purple-500 to-pink-500 h-screen"> -->
+    <main>
+      <TheWelcome />
+    </main>
+  </div>
+</template>
