dc72c4c6529540561016c19f846a1bdafdac7df6
Michael Saiba <michaelsaiba84@gmail.com>
2022-11-15 09:46:14 +0300

    styling menu done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <div class="opacity-50 bg-hero-pattern h-screen bg-cover bg-center object-cover">
+  <div class="opacity-50 bg-hero-pattern bg-fixed h-screen bg-cover bg-center object-cover">
   <!-- <div class=" bg-gradient-to-r from-purple-500 to-pink-500 h-screen"> -->
     <main>
       <TheWelcome />
