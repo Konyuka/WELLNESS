7d39fe57210e34d5d39cab73148a3850ff00ba54
Konyuka <michaelsaiba84@gmail.com>
2025-07-25 08:38:42 +0300

    Abel Munda

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -291,7 +291,8 @@ import { RouterLink } from 'vue-router'
                   <figcaption class="relative mt-6 flex items-center justify-between border-t border-slate-100 pt-6">
                     <div>
                       <div class="font-bold text-base text-slate-900">Michael Adwera</div>
-                      <div class="mt-1 text-sm text-red-800 font-bold">Chairman Africa Inuka Group / The VIP Luxury Hotels
+                      <div class="mt-1 text-sm text-red-800 font-bold">Chairman Africa Inuka Group / The VIP Luxury
+                        Hotels
                         Ltd.
                       </div>
                     </div>
@@ -301,35 +302,13 @@ import { RouterLink } from 'vue-router'
                   </figcaption>
                 </figure>
               </li>
-              <li>
-                <figure class="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10"><svg aria-hidden="true"
-                    width="105" height="78" class="absolute top-6 left-6 fill-slate-100">
-                    <path
-                      d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
-                    </path>
-                  </svg>
-                  <blockquote class="relative">
-                    <p class="text-lg tracking-tight text-slate-900">
-                      Excellently executed a National campaign for the official opening of AAR Hospital by the President
-                    </p>
-                  </blockquote>
-                  <figcaption class="relative flex items-center justify-between border-t border-slate-100 pt-4">
-                    <div>
-                      <div class="font-bold text-base text-slate-900">Steve Okeyo</div>
-                      <div class="mt-1 text-sm text-red-800 font-bold">CEO Hospital Holdings Investment (AAR Group)
-                      </div>
-                    </div>
-                    <div class="overflow-hidden rounded-full bg-slate-50"><img alt="" src="@/assets/steve.jpeg" width="56"
-                        height="56" decoding="async" data-nimg="1" class="h-14 w-14 object-cover" loading="lazy"
-                        style="color:transparent"></div>
-                  </figcaption>
-                </figure>
-              </li>
+
             </ul>
           </li>
           <li>
             <ul role="list" class="flex flex-col gap-y-6 sm:gap-y-8">
-              <li>
+
+              <!-- <li>
                 <figure class="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10"><svg aria-hidden="true"
                     width="105" height="78" class="absolute top-6 left-6 fill-slate-100">
                     <path
@@ -351,7 +330,33 @@ import { RouterLink } from 'vue-router'
                         style="color:transparent"></div>
                   </figcaption>
                 </figure>
+              </li> -->
+
+              <li>
+                <figure class="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10"><svg aria-hidden="true"
+                    width="105" height="78" class="absolute top-6 left-6 fill-slate-100">
+                    <path
+                      d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
+                    </path>
+                  </svg>
+                  <blockquote class="relative">
+                    <p class="text-lg tracking-tight text-slate-900">
+                      Excellently executed a National campaign for the official opening of AAR Hospital by the President
+                    </p>
+                  </blockquote>
+                  <figcaption class="relative flex items-center justify-between border-t border-slate-100 pt-4">
+                    <div>
+                      <div class="font-bold text-base text-slate-900">Steve Okeyo</div>
+                      <div class="mt-1 text-sm text-red-800 font-bold">CEO Hospital Holdings Investment (AAR Group)
+                      </div>
+                    </div>
+                    <div class="overflow-hidden rounded-full bg-slate-50"><img alt="" src="@/assets/steve.jpeg"
+                        width="56" height="56" decoding="async" data-nimg="1" class="h-14 w-14 object-cover"
+                        loading="lazy" style="color:transparent"></div>
+                  </figcaption>
+                </figure>
               </li>
+
               <li>
                 <figure class="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10"><svg aria-hidden="true"
                     width="105" height="78" class="absolute top-6 left-6 fill-slate-100">
@@ -372,9 +377,9 @@ import { RouterLink } from 'vue-router'
                       <div class="mt-1 text-sm text-red-800 font-bold">CEO Langu Green
                         Technologies</div>
                     </div>
-                    <div class="overflow-hidden rounded-full bg-slate-50"><img alt="" src="@/assets/odipo.jpeg" width="56"
-                        height="56" decoding="async" data-nimg="1" class="h-14 w-14 object-cover" loading="lazy"
-                        style="color:transparent"></div>
+                    <div class="overflow-hidden rounded-full bg-slate-50"><img alt="" src="@/assets/odipo.jpeg"
+                        width="56" height="56" decoding="async" data-nimg="1" class="h-14 w-14 object-cover"
+                        loading="lazy" style="color:transparent"></div>
                   </figcaption>
                 </figure>
               </li>
